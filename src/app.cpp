228552b4ee5787af2cca1859c1e9c986524b542d
He Ronghua <heronghua1989@126.com>
2025-02-09 21:16:15 +0800

    first commit

---
+++ src/app.cpp
@@ -0,0 +1,76 @@
+/*************************************************************************
+	 File Name: app.cpp
+	 Author: He Ronghua
+	 Mail: heronghua1989@126.com
+	 Created Time: Sun Feb  9 14:03:52 2025
+ ************************************************************************/
+
+#include <iostream>
+#include <string>
+
+#include <incbin.h>
+#define INCBIN_STYLE INCBIN_STYLE_SNAKE
+#define INCBIN_PREFIX g_
+
+/* Usage: INCBIN(<<LABLE>>, <<FILE>>)
+ *
+ * Symbols defined by INCBIN 
+ * ------------------------------------------ 
+ *  const unsigned char        g_asset_data[]  // g_<<LABLE>>_data 
+ *  const unsigned char* const g_asset_end;    // g_<<LABEL>>_end 
+ *  const unsinged int         g_asset_size;   // g_<<LABEL>>_size 
+ */
+INCBIN(asset, "../resource/data.txt");
+
+#define MEMORY_STREAM(label) \
+  memstream( (char*) ((g_ ## label ## _data)),              \
+             (char*) ((g_ ##label ## _data) + (g_ ## label ##_size)))
+
+/// Credits: https://stackoverflow.com/a/13059195
+/// https://stackoverflow.com/questions/13059091/
+struct membuf: std::streambuf {
+  membuf(char const* base, size_t size) {
+    char* p(const_cast<char*>(base));
+    this->setg(p, p, p + size);
+  }
+  virtual ~membuf() = default;
+};
+
+/// Credits: https://stackoverflow.com/a/13059195
+/// https://stackoverflow.com/questions/13059091/
+struct memstream: virtual membuf, std::istream {
+
+  memstream(char const* base, char* const end)
+    : membuf(base, reinterpret_cast<uintptr_t>(end) - reinterpret_cast<uintptr_t>(base) )
+    , std::istream(static_cast<std::streambuf*>(this)) { }
+
+  memstream(char const* base, size_t size)
+    : membuf(base, size)
+    , std::istream(static_cast<std::streambuf*>(this)) { }
+};
+
+// Read all lines from some input stream
+// and print on stdout.
+void print_lines(std::istream& is)
+{
+  std::string line;
+  int  n = 0;
+  while(std::getline(is, line) && n < 25)
+    std::cout << " line[" << n++ << "] = " << line << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+
+  std::puts("\n [EXPERIMENT 1] =>> Read directly from symbols ---");  
+  std::string text = std::string(g_asset_data, g_asset_data + g_asset_size);
+  std::cout << " Content of the asset file is " << std::endl;
+  std::cout << " => " << text << std::endl;
+
+  std::puts("\n [EXPERIMENT 2] =>> Read from memory stream -------");
+  auto is = MEMORY_STREAM(asset);
+  print_lines(is);
+
+  return 0;
+}
+
